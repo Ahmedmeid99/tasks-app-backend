073627554ff9e6e9011e36399f44eceb2c66ec6d
ahmedmeid99 <you@example.com>
2022-11-22 11:06:48 +0200

    add node version to package.json 03

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ app.use(cors())
 // const CONNECTION_URL = "mongodb+srv://ahmed:eid199963@cluster0.h575og5.mongodb.net/?retryWrites=true&w=majority"
 
 // const CONNECTION_URL = "mongodb://127.0.0.1:27017/todo-app-backend"
-const CONNECTION_URL = process.env.MONGODB_URL
+const CONNECTION_URL = "mongodb+srv://ahmed:eid199963@cluster0.h575og5.mongodb.net/?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 3000
 
 mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
-    .catch((e) => console.log(e.message))
+  .then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}`)))
+  .catch((e) => console.log(e.message))
 /*
  * C:/Users/Ezz/mongodb/bin/mongod.exe --dbpath C:/Users/Ezz/mongodb-data
 */
